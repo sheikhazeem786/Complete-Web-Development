0e66f3d18629ca88b5918d1d777d7be049947619
sheikhazeem786 <sheikhazeem141@gmail.com>
2022-03-01 15:38:20 +0530

    adding organizer task

--- File System Organiser/main.js
+++ File System Organiser/main.js
@@ -1,8 +1,15 @@
 let inputArr = process.argv.slice(2);
+const { dir } = require("console");
+let fs = require("fs");
+let path = require("path");
 // console.log(inputArr);
-
-
 let command = inputArr[0];
+utility.types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', 'xz'],
+    documents: ['docx', 'dox', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', 'deb']
+}
 switch (command) {
     case "tree":
         treefn();
@@ -25,8 +32,50 @@ function treefn(dirPath) {
 
 function organizefn(dirPath) {
     console.log("The command implemented for ", dirPath);
+    // 1. input -> directory path given
+    if (dirPath == undefined) {
+        console.log("Kindly enter the path");
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+            // 2. create -> organied files -> directory
+            let destPath = path.join(dirPath, "organized_files")
+            if (fs.existsSync(destPath) == false) {
+                fs.mkdirSync(destPath);
+            }
 
+        } else {
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+    organizeHelper(dirPath, destPath);
 }
+
+function organizeHelper(src, dest) {
+    // 3. identify categories of all the files present in that input directory
+    let childNames = fs.readdirSync(src);
+    // console.log(childNames);
+    for (let i = 0; i < childNames.length; i++) {
+        let childAddress = path.join(src, childNames[i]);
+        let isFile = fs.lstatSync(childAddress).isFile();
+        if (isFile) {
+            console.log(childNames[i]);
+            let category = getCategory(childNames[i]);
+            // 4. copy/cut files to that organized directory
+        }
+    }
+}
+
+function getCategory(name) {
+    let ext = path.extname(name);
+    console.log(ext);
+}
+
+
+
+
 
 function helpfn(dirPath) {
     console.log(`
